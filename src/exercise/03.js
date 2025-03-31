f9272ebf80e9d92a7cd711aa2cbf345f64ba3aa8
unbent <daniel.polan@hotmail.com>
2025-03-31 11:13:28 -0400

    Exercise 03 - Extra Credit 01

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -17,13 +17,23 @@ const CountProvider = ({children}) => {
   )
 }
 
+const useCount = () => {
+  const context = React.useContext(CountContext);
+
+  if (!context) {
+    throw new Error('useCount may only be used from within a (child of a) CountProvider');
+  } else {
+    return context;
+  }
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext);
+  const [count] = useCount
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
